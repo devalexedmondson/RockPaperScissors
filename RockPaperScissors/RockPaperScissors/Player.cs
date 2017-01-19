484441a369f8b26451e87c0b65c9e7ad0992fda8
devalexedmondson <alexedmondson20@gmail.com>
2017-01-19 16:11:40 -0600

    got ride of game file in order to see why getplayeroption method is running without being prompted

--- RockPaperScissors/RockPaperScissors/Player.cs
+++ RockPaperScissors/RockPaperScissors/Player.cs
@@ -5,4 +5,34 @@
         public string choice;
         public string name; 
     }
+    //public string GetPlayerOption()
+    //{
+    //    Console.WriteLine("Please type in your move, 'rock', 'paper', 'scissors', 'lizard', 'spock'.");
+    //    string playerChoice = Console.ReadLine();
+    //    if (playerChoice == "rock")
+    //    {
+    //        Console.WriteLine("rock");
+    //    }
+    //    else if (playerChoice == "paper")
+    //    {
+    //        Console.WriteLine("paper");
+    //    }
+    //    else if (playerChoice == "scissors")
+    //    {
+    //        Console.WriteLine("scissors");
+    //    }
+    //    else if (playerChoice == "lizzard")
+    //    {
+    //        Console.WriteLine("lizzard");
+    //    }
+    //    else if (playerChoice == "spock")
+    //    {
+    //        Console.WriteLine("spock");
+    //    }
+    //    else
+    //    {
+    //        GetPlayerOption();
+    //    }
+    //    return playerChoice;
+    //}
 }
